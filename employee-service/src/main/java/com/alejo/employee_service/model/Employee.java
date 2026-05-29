cd2a6d35a160c04a5f08b5e38b31b339058f6f83
alejoalbornoz <alejoalbornoz912@gmail.com>
2026-05-29 12:30:10 -0300

    Feat: Add Controller, Model and Repository to employee-service

---
+++ employee-service/src/main/java/com/alejo/employee_service/model/Employee.java
@@ -0,0 +1,16 @@
+package com.alejo.employee_service.model;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Employee {
+    private Long id;
+    private Long departmentId;
+    private String name;
+    private String position;
+
+
+}
